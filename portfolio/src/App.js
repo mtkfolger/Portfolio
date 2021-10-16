a6113e2e06dbeb13a8b658f6459823a61fff5b05
Michael Folger <folger@zoller-usa.com>
2021-10-15 21:40:58 -0500

    Created React App for Portfolio project

---
+++ portfolio/src/App.js
@@ -0,0 +1,12 @@
+import App from './App.css';
+import PortfolioApp from './components/PortfolioApp';
+
+function App() {
+  return (
+    <div className="portfolio-app">
+      <PortfolioApp />
+    </div>
+  );
+}
+
+export default App;
